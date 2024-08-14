f4694295b72e22af1211f65e70a96bc14b1e7b47
Matt Tompkins <matthewp.tompkins@gmail.com>
2024-08-14 07:40:29 +0100

    modify testimonial formatting

--- components/Home/HomeTestimonial.tsx
+++ components/Home/HomeTestimonial.tsx
@@ -4,7 +4,7 @@ import Grid from '../Grid/Grid';
 const HomeTestimonial: React.FC = (): JSX.Element => {
   const testimonialData: MT.TestimonialProps = {
     author: 'Zach Bridges',
-    role: 'Senior Software Engineer, Skillshare',
+    role: 'Coworker, Senior Software Engineer',
     text: "Matt Tompkins was one of my favorite peers while we worked together at Skillshare. My favorite feature of Matt is that he is a full-time student, always learning new things and digging deeper where he feels there's room for improvement. In practice, Matt works with the high bar of a professional, always seeking feedback and refusing to ship anything doesn't meet his high expectations. Matt\'s reputation with me is that \"good enough\" will not cut it with him.",
   };
   return (
--- components/Home/HomeTestimonialSlider.tsx
+++ components/Home/HomeTestimonialSlider.tsx
@@ -7,32 +7,32 @@ const HomeTestimonialSlider: React.FC = (): JSX.Element => {
   const testimonialData: MT.TestimonialProps[] = [
     {
       author: 'Carla Rucian',
-      role: 'SEO Manager, Colleague',
+      role: 'Coworker, SEO Manager',
       text: "I've had the pleasure of working with Matt for the past two and a half years, and I can confidently say that he is one of the most talented and dedicated engineers I've ever encountered. His expertise in Next.js is unmatched, and I've witnessed him tackle complex and challenging problems with a level of skill and perseverance that's truly inspiring... Beyond his technical abilities, Matt brings an infectious energy and wit to the team. He's the kind of person who makes work not only productive but enjoyable. His positive attitude, sense of humour, and willingness to help others make him an invaluable team member and a great person to be around.",
     },
     {
       author: 'Mandana Rafat',
-      role: 'Director of Organic Growth & Retention, Colleague',
+      role: 'Manager, Director of Organic Growth & Retention',
       text: "I've had the pleasure of working closely with Matt at Skillshare, where he has consistently demonstrated not only his technical prowess but also his exceptional ability to bridge the gap between technical and non-technical stakeholders. Matt has a unique talent for breaking down complex technical concepts into digestible insights, making collaboration with cross-functional teams seamless. What truly sets Matt apart is his proactive approach and dedication. He goes above and beyond, consistently delivering work of the highest quality. His problem-solving skills are top-notch, and he has a knack for identifying the most effective solutions that align with our objectives. Matt really excels at any role that requires technical expertise, collaboration, and a commitment to excellence.",
     },
     {
       author: 'Rebecca Dobbin',
-      role: 'Lead Product Manager, Manager',
+      role: 'Manager, Lead Product Manager',
       text: 'I highly recommend Matt for any engineering role! He is a strong engineer, produces beautiful work, and most importantly is warm, thoughtful, and always striving to improve. The level of self awareness Matt has is rare, and I deeply appreciate his ability to proactively check in as projects progress to see how things are going and what could improve. He is a strong communicator, and his clear and frequent updates on complex technical concepts are greatly appreciated amongst cross functional stakeholders, ranging from Marketing to Data. Matt brings a level of connection, heart, and wit that will make any team greater and I look forward to seeing what he does next!',
     },
     {
       author: 'Miguel Useche',
-      role: 'Senior Staff Engineer, Colleague',
+      role: 'Coworker, Senior Staff Engineer',
       text: "I enjoyed working with Matt on a large headless WordPress project, for which he was the primary leader. His expertise in frontend development, particularly with React, Next.js, and backend and frontend WordPress, is outstanding. Matt led the team confidently and generously shared his knowledge, elevating the skill set of everyone around him. His research skills are top-notch, and he's always well-prepared and informed. Beyond his technical abilities, Matt is incredibly polite, educated, and an absolute pleasure to work with. He's a true asset to any company.",
     },
     {
       author: 'Ryan Stanley',
-      role: 'SEO & Content Marketing Strategist, Colleague',
+      role: 'Coworker, SEO & Content Marketing Strategist',
       text: "Matt is undoubtedly one of the best engineers I've ever had the pleasure of working with... Matt is an exceptional collaborator, regardless of discipline or function on a team. I was particularly impressed by Matt's ability to experiment and adapt on the fly during design jams. His documentation skills are second to none, enabling our content team to quickly learn and apply new features efficiently. He excels at prioritizing effectively, expertly managing multiple company priorities simultaneously... Lastly, he's unafraid to ask the critical \"why\" questions that challenge the status quo, and he consistently offers well-thought-out, insightful recommendations.",
     },
     {
       author: 'Mark C.',
-      role: 'Employer, Vice President Digital & Web at Soliant',
+      role: 'Manager, Vice President Digital & Web at Soliant',
       text: "It's great working with Matt. He's a fast and organized developer. He's built me many WordPress sites from the ground up now and I plan on sending him as much as I can!  He's an excellent person to work with. Look no further, this is your guy if you need a WP/PHP developer.",
     },
     {
@@ -42,7 +42,7 @@ const HomeTestimonialSlider: React.FC = (): JSX.Element => {
     },
     {
       author: 'Farah A.',
-      role: 'Employer, Blind Renaissance',
+      role: 'Client, Blind Renaissance',
       text: 'Matt was incredible. His communication with us, his work and explanations of what he is doing, everything he completed for us was absolutely fantastic. I would 100% recommend Matt. Really pleasant to work with, work was always completed on time or even before, and he exceeded my expectations! 🙂 Absolutely fantastic work and great guy. We will definitely use Matt again for any of our web developer needs and again, I highly recommend him!',
     },
     {
