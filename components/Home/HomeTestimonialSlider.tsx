7e4ec80f4b29f57e433aa024100915b83d649065
Matt Tompkins <matthewp.tompkins@gmail.com>
2024-08-14 07:29:04 +0100

    add testimonials and modify sk dates

--- components/Home/HomeTestimonialSlider.tsx
+++ components/Home/HomeTestimonialSlider.tsx
@@ -7,18 +7,28 @@ const HomeTestimonialSlider: React.FC = (): JSX.Element => {
   const testimonialData: MT.TestimonialProps[] = [
     {
       author: 'Carla Rucian',
-      role: 'SEO Manager, Skillshare',
+      role: 'SEO Manager, Colleague',
       text: "I've had the pleasure of working with Matt for the past two and a half years, and I can confidently say that he is one of the most talented and dedicated engineers I've ever encountered. His expertise in Next.js is unmatched, and I've witnessed him tackle complex and challenging problems with a level of skill and perseverance that's truly inspiring... Beyond his technical abilities, Matt brings an infectious energy and wit to the team. He's the kind of person who makes work not only productive but enjoyable. His positive attitude, sense of humour, and willingness to help others make him an invaluable team member and a great person to be around.",
     },
     {
       author: 'Mandana Rafat',
-      role: 'Director of Organic Growth & Retention, Skillshare',
+      role: 'Director of Organic Growth & Retention, Colleague',
       text: "I've had the pleasure of working closely with Matt at Skillshare, where he has consistently demonstrated not only his technical prowess but also his exceptional ability to bridge the gap between technical and non-technical stakeholders. Matt has a unique talent for breaking down complex technical concepts into digestible insights, making collaboration with cross-functional teams seamless. What truly sets Matt apart is his proactive approach and dedication. He goes above and beyond, consistently delivering work of the highest quality. His problem-solving skills are top-notch, and he has a knack for identifying the most effective solutions that align with our objectives. Matt really excels at any role that requires technical expertise, collaboration, and a commitment to excellence.",
     },
     {
       author: 'Rebecca Dobbin',
-      role: 'Lead Product Manager, Skillshare',
-      text: "I highly recommend Matt for any engineering role! He is a strong engineer, produces beautiful work, and most importantly is warm, thoughtful, and always striving to improve. The level of self awareness Matt has is rare, and I deeply appreciate his ability to proactively check in as projects progress to see how things are going and what could improve. He is a strong communicator, and his clear and frequent updates on complex technical concepts are greatly appreciated amongst cross functional stakeholders, ranging from Marketing to Data. Matt brings a level of connection, heart, and wit that will make any team greater and I look forward to seeing what he does next!",
+      role: 'Lead Product Manager, Manager',
+      text: 'I highly recommend Matt for any engineering role! He is a strong engineer, produces beautiful work, and most importantly is warm, thoughtful, and always striving to improve. The level of self awareness Matt has is rare, and I deeply appreciate his ability to proactively check in as projects progress to see how things are going and what could improve. He is a strong communicator, and his clear and frequent updates on complex technical concepts are greatly appreciated amongst cross functional stakeholders, ranging from Marketing to Data. Matt brings a level of connection, heart, and wit that will make any team greater and I look forward to seeing what he does next!',
+    },
+    {
+      author: 'Miguel Useche',
+      role: 'Senior Staff Engineer, Colleague',
+      text: "I enjoyed working with Matt on a large headless WordPress project, for which he was the primary leader. His expertise in frontend development, particularly with React, Next.js, and backend and frontend WordPress, is outstanding. Matt led the team confidently and generously shared his knowledge, elevating the skill set of everyone around him. His research skills are top-notch, and he's always well-prepared and informed. Beyond his technical abilities, Matt is incredibly polite, educated, and an absolute pleasure to work with. He's a true asset to any company.",
+    },
+    {
+      author: 'Ryan Stanley',
+      role: 'SEO & Content Marketing Strategist, Colleague',
+      text: "Matt is undoubtedly one of the best engineers I've ever had the pleasure of working with... Matt is an exceptional collaborator, regardless of discipline or function on a team. I was particularly impressed by Matt's ability to experiment and adapt on the fly during design jams. His documentation skills are second to none, enabling our content team to quickly learn and apply new features efficiently. He excels at prioritizing effectively, expertly managing multiple company priorities simultaneously... Lastly, he's unafraid to ask the critical \"why\" questions that challenge the status quo, and he consistently offers well-thought-out, insightful recommendations.",
     },
     {
       author: 'Mark C.',
@@ -48,7 +58,7 @@ const HomeTestimonialSlider: React.FC = (): JSX.Element => {
     {
       author: 'Marianna R.',
       role: 'Client',
-      text: 'Matt was great. He set out a great plan from the beginning and executed it perfectly. He was extremely responsive and finished the site before I could provide him copy. He also sent me a tutorial on how to edit the site myself. I\'d give him 10 stars if I could. Consider this my highest recommendation.',
+      text: "Matt was great. He set out a great plan from the beginning and executed it perfectly. He was extremely responsive and finished the site before I could provide him copy. He also sent me a tutorial on how to edit the site myself. I'd give him 10 stars if I could. Consider this my highest recommendation.",
     },
   ];
 
